3324b7a7c687eebc901b90d27a19020547d85ef6
Taito99 <amadeuszbujalski300@gmail.com>
2024-04-27 00:05:52 +0200

    modele dodane do bazy danych

--- pets/models.py
+++ pets/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Pets(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    breed = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
--- visit/models.py
+++ visit/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from pets.models import Pets
+from users.models import CustomUser
+
 
 # Create your models here.
+class Visit(models.Model):
+    pet = models.ForeignKey(Pets, on_delete=models.CASCADE, related_name='visits')
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='visits')
+    date = models.DateField()
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.date} - {self.pet.name} - {self.owner.username}"
