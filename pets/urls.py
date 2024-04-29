91594521e7d9a1999b5e3fbc83a1b3ea37215dcc
Taito99 <amadeuszbujalski300@gmail.com>
2024-04-29 10:53:02 +0200

    aktualizacja urls pets

--- pets/urls.py
+++ pets/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import get_pet, pet_detail, create_pet, update_pet, delete_pet
 
 urlpatterns = [
-    path('pets/<int:pk>/', get_pet, name='pet-list'),
+    path('pets/getpet/<int:pk>/', get_pet, name='get-pet'),
     path('pets/<int:pk>/', pet_detail, name='pet-detail'),
     path('pets/create/', create_pet, name='pet-create'),
     path('pets/<int:pk>/update/', update_pet, name='pet-update'),
