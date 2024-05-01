f0e4fd4418aaf0008ae483603ee3b0671265cb54
Adrian Krauze <adriankrauze85@gmail.com>
2024-05-01 13:37:50 +0200

    dodane profil usera #3 ZMAINA

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .serializers import ProfileSerializer
 
-# Create your views here.
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]  # Ustawienie wymagania autoryzacji
+
+    def get(self, request):
+        user_profile = Profile.objects.get(user=request.user)  # Pobranie profilu użytkownika zalogowanego
+        serializer = ProfileSerializer(user_profile)  # Serializacja profilu
+        return Response(serializer.data)
+
+from django.shortcuts import render
+from .models import Profile
+
+def user_profile(request):
+    user_profile = Profile.objects.get(user=request.user)
+    return render(request, 'profile.html', {'profile': user_profile})
+
