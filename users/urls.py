2abf2f0b40062da17888a8f3327f43bd6610aa4b
Adrian Krauze <adriankrauze85@gmail.com>
2024-05-01 17:53:02 +0200

    17:52 01.05 dodatkowe recenzje dodanie rest api

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import register, login, logout, CustomPasswordResetView
 from django.contrib.auth import views as auth_views
 
-
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login, name='login'),
@@ -11,4 +10,5 @@ urlpatterns = [
     path('reset_password/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    # Dodaj inne adresy URL, które masz
 ]
