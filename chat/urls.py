c8483fd2ce317cf8ce6e87591adb2616defcc983
adrian <adrianrkrauze85@gmail.com>
2024-05-08 20:07:33 +0200

    08.05 2007

--- chat/urls.py
+++ chat/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Ścieżka do widoku czatu
-    path('chat/', views.chat_room, name='chat_room'),
     # Ścieżka do widoku czatu z konkretnym użytkownikiem
-    path('chat/<int:receiver_id>/', views.chat_with_contact, name='chat_with_contact'),
     path('chatcreate/', views.create_chat, name='chat'),
+    path('chats/', views.user_chats_list, name='chatlist'),
+    path('chats/<uuid:chat_id>/messages/', views.chat_messages, name='chat-messages'),
+
+
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,77 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .models import Message
+from .models import Message, Chat
 from users.models import CustomUser  # Załóżmy, że CustomUser to model użytkownika w Twojej aplikacji
-from .serializers import ChatSerializer
-
-
-@api_view(['POST'])
-@login_required
-def chat_room(request):
-    if request.method == 'POST':
-        # Obsługa wysłania nowej wiadomości
-        sender = request.user
-        receiver_id = request.POST.get('receiver_id')
-        content = request.POST.get('content')
-        receiver = CustomUser.objects.get(id=receiver_id)
-        # Zapisz nową wiadomość
-        Message.objects.create(sender=sender, receiver=receiver, content=content)
-        # Przekieruj z powrotem do widoku czatu
-        return redirect('chat_room')
-
-    # Pobierz listę kontaktów w zależności od roli użytkownika
-    if request.user.is_staff:
-        # Jeśli użytkownik jest administratorem, pobierz listę wszystkich użytkowników
-        contacts = CustomUser.objects.exclude(id=request.user.id)
-    else:
-        # W przeciwnym razie, pobierz listę administratorów
-        contacts = CustomUser.objects.filter(is_staff=True)
-
-    chosen_contact = request.GET.get('receiver')
-
-    if chosen_contact:
-        # Jeśli został wybrany kontakt, przekieruj użytkownika do nowej strony z czatem
-        return redirect('chat_with_contact', receiver_id=chosen_contact)
-
-    return render(request, 'chat/chat_room.html', {'contacts': contacts, 'chosen_contact': None})
-
-
-@login_required
-def chat_with_contact(request, receiver_id):
-    # Pobierz kontakt na podstawie ID odbiorcy
-    receiver = CustomUser.objects.get(id=receiver_id)
-
-    # Pobierz wszystkie wiadomości między aktualnym użytkownikiem a wybranym użytkownikiem
-    sender_messages = Message.objects.filter(sender=request.user, receiver=receiver).order_by('timestamp')
-    receiver_messages = Message.objects.filter(sender=receiver, receiver=request.user).order_by('timestamp')
-
-    # Połącz oba zbiory wiadomości i posortuj je według czasu
-    messages = list(sender_messages) + list(receiver_messages)
-    messages.sort(key=lambda x: x.timestamp)
-
-    # Utwórz listę osób, z którymi ostatnio pisał użytkownik/administrator
-    last_contacts = []
-    for message in messages:
-        if message.sender != request.user and message.sender not in last_contacts:
-            last_contacts.append(message.sender)
-        elif message.receiver != request.user and message.receiver not in last_contacts:
-            last_contacts.append(message.receiver)
-
-    # Wyświetlaj nazwę użytkownika/administratora, który wysłał ostatnią wiadomość, aby użytkownik mógł szybko
-    # zidentyfikować Ostatnia wiadomość będzie wiadomością z najnowszym znacznikiem czasu
-    last_message = messages[-1] if messages else None
-    last_sender = last_message.sender if last_message else None
-
-    # Sprawdź, czy są jakieś nieprzeczytane wiadomości
-    unread_messages = Message.objects.filter(receiver=request.user, is_read=False)
-
-    return render(request, 'chat/chat_with_contact.html',
-                  {'receiver': receiver, 'messages': messages, 'last_sender': last_sender,
-                   'unread_messages': unread_messages})
+from .serializers import ChatSerializer, ChatListSerializer, MessageSerializer
 
 
 @api_view(['POST'])
@@ -84,6 +19,36 @@ def create_chat(request):
     else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(['GET'])
+@login_required
+def user_chats_list(request):
+    # Get the current user from the request
+    user = request.user
+    # Filter chats where the current user is a participant
+    chats = Chat.objects.filter(participants=user)
+    # Serialize the data
+    serializer = ChatListSerializer(chats, many=True)
+    # Return the data
+    return Response(serializer.data)
 
+@api_view(['GET', 'POST'])
+@login_required
+def chat_messages(request, chat_id):
+    # Retrieve the chat object, ensuring it exists.
+    chat = get_object_or_404(Chat, id=chat_id)
 
+    if request.method == 'GET':
+        # Retrieve all messages from the chat
+        messages = Message.objects.filter(chat=chat)
+        serializer = MessageSerializer(messages, many=True)
+        return Response(serializer.data)
 
+    elif request.method == 'POST':
+        # Create a new message in the chat
+        serializer = MessageSerializer(data=request.data)
+        if serializer.is_valid():
+            # Set the sender to the current user and the chat to the retrieved chat
+            serializer.save(sender=request.user, chat=chat)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
